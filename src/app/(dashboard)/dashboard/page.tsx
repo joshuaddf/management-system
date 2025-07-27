4d8c67c4517a9eef4772b59d384776f1a63e87cf
joshuaddf <jkdodofoli@gmail.com>
2025-07-27 23:41:32 +0000

    changed the content

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,21 +1,29 @@
 import Link from 'next/link'
-import React from 'react'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { getKindeServerSession, LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import { Button } from '@/components/ui/button';
 
 const page = async () => {
 
-  const {  getUser } = await getKindeServerSession();
+  const { getUser } = await getKindeServerSession();
   const user = await getUser();
 
   return (
-    <div className='flex items-center justify-center h-screen'>
+    <div className='flex items-center justify-center h-screen px-4'>
       <div className="flex flex-col">
-        <span className='text-xl'><Link href="/">Home</Link></span>
-        <div className="text-4xl font-semibold">This is the dashboard</div>
+        <div className="flex items-center justify-between pb-7">
+          <span className='text-sm underline'><Link href="/">Home</Link></span>
+          <Button variant='secondary'>
+            <LogoutLink className='text-sm underline'>Logout</LogoutLink>
+          </Button>
+        </div>
+        <div className="text-3xl font-bold">You are logged in</div>
         <div className="flex flex-col">
-          <span>{user?.email}</span>
-          <span>{user?.given_name}</span>
-          <span>{user?.family_name}</span>
+          <div className="py-4">
+            <p className='font-semibold text-lg opacity-60'>Hello {user?.given_name} {user?.family_name}, you are successfully logged in.</p>
+          </div>
+          <div className="flex flex-col items-end">
+            <span className=' opacity-65 font-bold text-sm'>{user?.email}</span>
+          </div>
         </div>
       </div>
     </div>
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,10 +3,10 @@ import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen px-4">
       <main>
-        <h1 className="text-3xl font-bold underline">School management system</h1>
-        <p className="text-lg py-6">This is a simple school management system</p>
+        <h1 className="text-3xl font-bold w-full">School management system</h1>
+        <p className="text-lg py-4 font-semibold opacity-65">This is a simple school management system</p>
         <div className="flex items-center justify-center gap-6">
           <Button variant="default">
             <RegisterLink>Sign up</RegisterLink>
